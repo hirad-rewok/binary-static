70ac56f7f25522a0795dda26a43ce63d05ef247e
hirad-rework <hirad@re-work.dev>
2021-11-24 10:25:19 +0330

    fix--onLoad Banner will not show for uk users

--- src/javascript/app/base/page.js
+++ src/javascript/app/base/page.js
@@ -136,8 +136,8 @@ const Page = (() => {
                 const mlt_check = ClientBase.get('landing_company_shortcode') === 'malta';
                 const mf_check = ClientBase.get('landing_company_shortcode') === 'maltainvest';
                 const virtual_account = Client.get('landing_company_shortcode') === 'virtual';
-                if (!is_iom_client) RedirectBanner.loginOnLoad();
-                if (is_uk_residence && Client.hasAccountType('gaming')) {
+                if (!is_iom_client && !is_uk_residence) RedirectBanner.loginOnLoad();
+                if (is_uk_residence && Client.hasAccountType('gaming') || virtual_account) {
                     CloseBanner.onLoad();
                     ClosePopup.loginOnLoad();
                     CloseBanner.onLoad();
